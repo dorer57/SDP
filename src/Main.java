3e52f423b062e4146269defdc7d05793c36ce09f
6e6ra <kebab7082@gmail.com>
2026-09-13 16:21:31 +0500

    wadsadw

--- src/Main.java
+++ src/Main.java
@@ -9,6 +9,7 @@ public class Main {
 
         toppings.add("Pepperoni");
         toppings.add("Mushrooms");
+        toppings.add("waddsad")
 
         Pizza pizza = new Pizza(
                 "Large",
